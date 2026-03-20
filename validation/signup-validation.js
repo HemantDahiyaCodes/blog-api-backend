2c6c7fcaf484323dc9fc310394968c8a6e9be294
HemantDahiyaCodes <HemantDahiyaCodes@gmail.com>
2026-03-20 04:47:38 +0000

    Add a regex to validate password

--- validation/signup-validation.js
+++ validation/signup-validation.js
@@ -2,7 +2,7 @@ import { body } from "express-validator";
 import { prisma } from "../prismaClientConfig.js";
 
 export const validationRules = [
-  body("username").notEmpty().isLength(2, 15),
+  body("username").notEmpty().isLength({min: 5, max: 25}),
   body("username").custom(async (value, {req}) => {
     console.log("The username in req is: ", req.body.username);
     const user = await prisma.user.findUnique({
@@ -13,6 +13,6 @@ export const validationRules = [
         throw new Error(`${user.username} already exists`);
     }
   }),
-  body("password").isAlphanumeric().isLength(5, 15),
+  body("password").isLength({min: 5, max: 35}).matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d_]+$/).withMessage("Password must contain a mix of letters, at least one number"),
   body("email").isEmail().notEmpty(),
 ];
