d1ddd3c1bc7d3a53c10c6c26cb90f1c6d1705db5
HemantDahiyaCodes <HemantDahiyaCodes@gmail.com>
2026-03-22 03:54:27 +0000

    change cjs imports to es6 modules Fixed prismaclient import Configure passport-jwt

--- auth/JWTStrategy.js
+++ auth/JWTStrategy.js
@@ -1,27 +1,24 @@
-const jwtStrategy = require("passport-jwt").Strategy;
-const passport = require("passport");
-const extractJwt = require("passport-jwt").ExtractJwt;
-const dotenv = require("dotenv").config();
-const { PrismaClient } = require("../generated/prisma");
-const prisma = new PrismaClient();
+import passport from "passport";
+import { Strategy as JwtStrategy } from "passport-jwt";
+import { ExtractJwt as extractJwt } from "passport-jwt";
+import { prisma } from "../prismaClientConfig.js";
 
 const options = {
   jwtFromRequest: extractJwt.fromAuthHeaderAsBearerToken(), // Get the token from "authorization" header.
   secretOrKey: process.env.SECRET_KEY,
 };
 
-module.exports = passport.use(
-  new jwtStrategy(options, async function (jwt_paylaod, done) {
-    console.log("The user in JWT strategy is: ", jwt_paylaod.user.id);
+passport.use(
+  new JwtStrategy(options, async function (jwt_payload, done) {
+    console.log("The user in JWT strategy is: ", jwt_payload.user.id);
     const user = await prisma.user.findUnique({
-      where: { id: jwt_paylaod.user.id },
+      where: { id: jwt_payload.user.id },
     });
-    console.log("The user in JWT strategy is: ", user);
 
     if (!user) {
       return done(null, false);
     }
 
     return done(null, user);
-  })
+  }),
 );
