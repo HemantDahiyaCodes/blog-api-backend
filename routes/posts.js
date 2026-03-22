960adca0988348ffb060791ad6edd54720e879a4
HemantDahiyaCodes <HemantDahiyaCodes@gmail.com>
2026-03-22 03:58:22 +0000

    Add jwt authentication in the POST route

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import * as postController from "../controllers/postController.js";
 import { createComment } from "../controllers/commentController.js";
+import passport from "passport";
+import "../auth/JWTStrategy.js"
 
 const posts = Router();
 
@@ -8,7 +10,11 @@ posts.get("/", postController.allPosts);
 
 posts.get("/:postId", postController.getPost);
 
-posts.post("/", postController.createPost);
+posts.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  postController.createPost,
+);
 
 posts.use("/:postId/comments", createComment);
 
