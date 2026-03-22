d5d4dd9cae8233ba85d5c5ecec5a7e013ff2ee8f
HemantDahiyaCodes <HemantDahiyaCodes@gmail.com>
2026-03-22 01:49:48 +0000

    Extract the comment logic to its own route

--- routes/posts.js
+++ routes/posts.js
@@ -1,31 +1,15 @@
 import { Router } from "express";
 import * as postController from "../controllers/postController.js";
+import { createComment } from "../controllers/commentController.js";
+
 const posts = Router();
 
+posts.get("/", postController.allPosts);
 
-posts.get(
-  "/",
-  postController.allPosts
-);
+posts.get("/:postId", postController.getPost);
 
-posts.get(
-  "/:postId",
-  postController.getPost
-);
+posts.post("/", postController.createPost);
 
-posts.post(
-  "/:postId/comments",
-  postController.postComment
-);
+posts.use("/:postId/comments", createComment);
 
-posts.post(
-  "/",
-  postController.createPost
-);
-
-posts.delete(
-  "/:postId/:commentId/",
-  postController.deleteComment
-);
-
-export {posts}
+export { posts };
