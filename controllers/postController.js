42136be4e67b18fea81fd85a351b269f413fb222
HemantDahiyaCodes <HemantDahiyaCodes@gmail.com>
2026-03-22 01:47:43 +0000

    Remove env variable while fetching all posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,10 +1,19 @@
 import { prisma } from "../prismaClientConfig.js";
 
+
+async function allPosts(req, res) {
+  const posts = await prisma.post.findMany();
+
+  res.json({posts})
+}
+
+
 async function getPost(req, res) {
   const { postId } = req.params;
   console.log("Post id in req.params is: ", postId);
+
   const post = await prisma.post.findFirst({
-    where: { id: parseFloat(postId), authorId: 2 },
+    where: { id: parseFloat(postId)},
     include: {
       comments: true,
     },
@@ -14,56 +23,16 @@ async function getPost(req, res) {
   res.json({ post });
 }
 
-async function postComment(req, res) {
-  const username = req.user;
-  console.log("User is: ", username);
-
-  const { postId } = req.params;
-  console.log("The id of the post is: ", postId);
-
-  const commentInReq = req.body;
-  console.log("The comment is: ", commentInReq);
-
-  const comment = await prisma.comment.create({
-    data: {
-      userId: username.id,
-      postId: parseFloat(postId),
-      content: commentInReq.comment,
-      username: username.username,
-    },
-  });
-
-  res.json({ comment: comment });
-}
-
-async function allPosts(req, res) {
-  const userInReq = req.user;
-  console.log("User in postcontroller: ", userInReq);
-
-  const username = "hemant";
-
-  const user = await prisma.user.findUnique({
-    where: {
-      username: username,
-    },
-
-    include: {
-      posts: true,
-    },
-  });
-
-  console.log("Posts of the user: ", user.posts);
-
-  res.json({ posts: user.posts, userInReq });
-}
-
 async function createPost(req, res) {
   const postInReq = req.body;
   console.log("The post in request is: ", postInReq);
 
+  const userInReq = req.user;
+
   const user = await prisma.user.findUnique({
     where: {
-      id: 2,
+      username: userInReq.username,
+      isAdmin: true
     }
   })
 
@@ -78,35 +47,8 @@ async function createPost(req, res) {
   res.json(post);
 }
 
-async function deleteComment(req, res) {
-  const postId = req.params.postId;
-  console.log(postId)
-
-  const commentId = req.params.commentId;
-  console.log(commentId)
-
-  const post = await prisma.post.findFirst({
-    where: {id: parseFloat(postId)},
-    include: {
-      comments: true
-    }
-  })
-
-  const comment = await prisma.comment.delete({
-    where: {
-      id: parseFloat(commentId),
-      postId: post.id,
-    }
-  })
-
-  console.log("Comment Deleted");
-  console.log("All comments", post.comments);
-}
-
 export {
   getPost,
-  postComment,
   allPosts,
   createPost,
-  deleteComment
 }
