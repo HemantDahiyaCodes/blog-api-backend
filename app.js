354b67ecadce71f5e8dc70c8153ec0eec0164af0
HemantDahiyaCodes <HemantDahiyaCodes@gmail.com>
2026-03-22 02:16:44 +0000

    Extract cors origin to env

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import cors from "cors";
 import "dotenv/config";
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_01,
   credentials: true,
   metohds: 'GET, POST',
 }
