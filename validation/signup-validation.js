aea46d9bbdef68c687117a71f4819f5f3c3e0a42
HemantDahiyaCodes <HemantDahiyaCodes@gmail.com>
2026-03-23 04:32:06 +0000

    Fix the value not being ready in db

--- validation/signup-validation.js
+++ validation/signup-validation.js
@@ -4,14 +4,15 @@ import { prisma } from "../prismaClientConfig.js";
 export const validationRules = [
   body("username").notEmpty().isLength({min: 5, max: 25}),
   body("username").custom(async (value, {req}) => {
-    console.log("The username in req is: ", req.body.username);
     const user = await prisma.user.findUnique({
-      where: { username: req.body.username },
+      where: { username: value},
     });
 
     if(user) {
         throw new Error(`${user.username} already exists`);
     }
+
+    return true;
   }),
   body("password").isLength({min: 5, max: 35}).matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d_]+$/).withMessage("Password must contain a mix of letters, at least one number"),
   body("email").isEmail().notEmpty(),
