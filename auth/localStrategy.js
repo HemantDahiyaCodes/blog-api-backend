87934b9eec4973cd6cf96f59a90e3d6e88eb3ad9
HemantDahiyaCodes <HemantDahiyaCodes@gmail.com>
2026-03-24 01:06:21 +0000

    Remove testing logs and extract the passport config

--- auth/localStrategy.js
+++ auth/localStrategy.js
@@ -1,31 +1,25 @@
-import passport from "passport";
-import LocalStrategy from "passport-local";
+import { Strategy as LocalStrategy } from "passport-local";
 import { prisma } from "../prismaClientConfig.js";
 import bcrypt from "bcryptjs";
 
-export default passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    console.log("Strategy hit with: ", username, password)
+export const localStrategy = new LocalStrategy(
+  async (username, password, done) => {
     try {
       const user = await prisma.user.findUnique({
         where: { username: username },
       });
 
-      console.log("User found: ", user);
       if (!user) {
         return done(null, false, { message: "Incorrect username" });
       }
 
-      // return if password is incorrect
       const matchPassword = await bcrypt.compare(password, user.password);
       if (!matchPassword) {
         return done(null, false, { message: "Incorrect password" });
       }
-
-      console.log(`Sending user.... named ${user.username}`);
       return done(null, user);
     } catch (err) {
       return done(err);
     }
-  })
+  },
 );
