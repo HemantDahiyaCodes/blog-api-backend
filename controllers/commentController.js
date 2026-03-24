8172b62d00004186489ee776e0908652e8557d79
HemantDahiyaCodes <HemantDahiyaCodes@gmail.com>
2026-03-24 05:00:27 +0000

    Remove testing logs Destructured properties of req to maintain cleaner approach Remove user object returning from json

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -5,25 +5,21 @@ async function createComment(req, res) {
   try {
     const result = validationResult(req);
 
-    console.log("The user is: ", req.user);
-
     if (result.isEmpty()) {
-      const user = req.user;
-
       const { postId } = req.params;
-      const commentContent = req.body;
-      console.log(commentContent);
+      const {content} = req.body;
+      const {user} = req;
 
       const comment = await prisma.comment.create({
         data: {
-          content: commentContent.content,
+          content,
           userId: parseInt(user.id),
           postId: parseInt(postId),
           Username: user.username,
         },
       });
 
-      res.json({comment, user})
+      res.json({comment})
     }
   } catch (err) {
     throw new Error(err);
