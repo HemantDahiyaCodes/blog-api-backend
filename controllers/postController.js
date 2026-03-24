1e362b89aa75c3c4b20c3147a25e85a0bde36385
HemantDahiyaCodes <HemantDahiyaCodes@gmail.com>
2026-03-24 04:19:30 +0000

    Clean up the logic of creating and getting a post Added error handling in case of an error Replace including the full user object to just usrename and id destructured user and post's properties to maintain clean code

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,50 +1,55 @@
 import { prisma } from "../prismaClientConfig.js";
 
 async function allPosts(req, res) {
-  const posts = await prisma.post.findMany();
+  try {
+    const posts = await prisma.post.findMany();
 
-  res.json({ posts });
+    res.json({ posts });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function getPost(req, res) {
-  const { postId } = req.params;
+  try {
+    const { postId } = req.params;
 
-  const post = await prisma.post.findFirst({
-    where: { id: parseFloat(postId) },
-    include: {
-      comments: true,
-      author: true,
-    },
-  });
+    const post = await prisma.post.findFirst({
+      where: { id: parseFloat(postId) },
+      include: {
+        author: {
+          select: {
+            username: true,
+            id: true,
+          },
+        },
+        comments: true,
+      },
+    });
 
-  const user = req.user;
-
-  res.json({ post });
+    return res.json(post);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function createPost(req, res) {
-  const postInReq = req.body;
-  console.log("The post in request is: ", postInReq);
+  try {
+    const { title, description, content } = req.body;
+    const { user } = req;
 
-  const userInReq = req.user; // Stores the user in req for verifying
-
-  const user = await prisma.user.findUnique({
-    where: {
-      username: userInReq.username,
-      isAdmin: true,
-    },
-  });
-
-  const post = await prisma.post.create({
-    data: {
-      authorId: parseInt(user.id),
-      title: postInReq.title,
-      content: postInReq.content,
-      description: postInReq.description,
-    },
-  });
-
-  res.json(post);
+    const post = await prisma.post.create({
+      data: {
+        authorId: parseInt(user.id),
+        title,
+        description,
+        content,
+      },
+    });
+    return res.json(post);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export { getPost, allPosts, createPost };
